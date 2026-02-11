3616d9ce8691f852971dccef16f1e97c25ab6a64
VietLinh1104 <linhtx2004@gmail.com>
2026-02-12 00:18:48 +0700

    [init] alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 
 // https://vite.dev/config/
@@ -8,5 +9,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    tsconfigPaths(),
   ],
 })
