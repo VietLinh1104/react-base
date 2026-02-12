5aa3659252af5d4431e4e00d6c3dcb49d04f2395
VietLinh1104 <linhtx2004@gmail.com>
2026-02-12 20:09:00 +0700

    [config] alias config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +11,10 @@ export default defineConfig({
     tailwindcss(),
     tsconfigPaths(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
 })
